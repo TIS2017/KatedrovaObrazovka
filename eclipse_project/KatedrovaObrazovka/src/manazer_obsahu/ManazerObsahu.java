b5428cea856116f4a1b9a00e1cf8dec507a21a15
Skylonxe <ondrej.hrus@gmail.com>
2017-11-22 02:25:28 +0100

    Nahrany projek, ktory sluzil ako zaklad pre diagram
    Pridany aj projekt a png diagramu v zlozke documentation.

---
+++ eclipse_project/KatedrovaObrazovka/src/manazer_obsahu/ManazerObsahu.java
@@ -0,0 +1,17 @@
+package manazer_obsahu;
+
+public class ManazerObsahu {
+	
+	public void skontrolujAktualnost()
+	{
+		float x = NastaveniaManazeraObsahu.frekvenciaAktualizacie;
+		
+		aktualizuj();
+	}
+	
+	void aktualizuj()
+	{
+		
+	}
+
+}
---
+++ eclipse_project/KatedrovaObrazovka/src/obrazovka/Video.java
@@ -0,0 +1,10 @@
+package obrazovka;
+
+import javafx.scene.canvas.Canvas;
+
+public class Video extends Canvas {
+	public void vykresli()
+	{
+		
+	}
+}
