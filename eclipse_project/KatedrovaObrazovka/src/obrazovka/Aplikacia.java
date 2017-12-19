a6f6da4b0ecb89de552afbbbeaa24bb210b7b541
Skylonxe <ondrej.hrus@gmail.com>
2017-12-19 19:16:28 +0100

    Pridany ini test a aplikacne okno
    Zahrna aj kniznicu ini4j

---
+++ eclipse_project/KatedrovaObrazovka/src/obrazovka/Aplikacia.java
@@ -0,0 +1,53 @@
+package obrazovka;
+
+import java.io.File;
+
+import org.ini4j.Ini;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import manazer_obsahu.ManazerObsahu;
+
+public class Aplikacia extends Application {
+	
+	/**
+	 * Root java fx element.
+	 */
+	public static BorderPane root;
+	
+	/**
+	 * Application scale.
+	 */
+	public final static int WINDOW_SIZE = 900;
+	
+	public final static String TITLE = "Katedrova obrazovka";
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle(TITLE);
+		root = new BorderPane();
+
+		primaryStage.setScene(new Scene(root, WINDOW_SIZE, WINDOW_SIZE));
+		primaryStage.show();
+		
+		Ini ini = new Ini(new File("test.ini"));
+		System.out.println(ini.get("testnadpis", "kluc"));
+	}
+	
+	public void nacitajObsah()
+	{
+
+	}
+	
+	public void vykresliObsah()
+	{
+
+	}
+}
